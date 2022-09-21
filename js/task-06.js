a2935b7c565396275a340076ddd9e3de0985717c
Ivan Sytnik <ivan.sytnik02@gmail.com>
2022-09-21 03:29:45 +0300

    7 tasks finished

---
+++ js/task-06.js
@@ -0,0 +1,15 @@
+const textInput = document.querySelector('#validation-input');
+textInput.addEventListener("blur", () => {
+    if(textInput.value.length == textInput.dataset.length) {
+     textInput.classList.add( "valid");
+     if(textInput.classList.contains("invalid")) {
+        textInput.classList.remove("invalid");
+     }
+    }
+    else {
+        textInput.classList.add("invalid");
+        if(textInput.classList.contains("valid")) {
+            textInput.classList.remove("valid");
+         }
+    }
+  });
