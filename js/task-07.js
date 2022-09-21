22a32bdfdc1c57d4a961987622254e90b06e86a5
Ivan Sytnik <ivan.sytnik02@gmail.com>
2022-09-22 00:29:46 +0300

    tasks ended

--- js/task-07.js
+++ js/task-07.js
@@ -1,7 +1,10 @@
 const textInput = document.querySelector('#font-size-control');
 const text = document.querySelector('#text');
-console.log(textInput.value);
 text.style.fontSize = `${textInput.value}px`;
-textInput.addEventListener( () => {  
-    text.style.fontSize = `${textInput.value}px`;
-  });
+
+textInput.addEventListener("input", () =>{  
+  text.style.fontSize = `${textInput.value}px`;
+}
+  );
+  
+  
--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,32 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const input = document.querySelector('input');
+const createBtn = document.querySelector('button[data-create]');
+const clearBtn = document.querySelector('button[data-destroy]');
+const table = document.querySelector('#controls');
+const box = document.querySelector('#boxes');
+const array = [];
+function createBoxes(amount) {
+  for(let i = 1; i <= amount; i += 1) {
+    const pisun = (i+2)*10;
+    array.push(`<div width="${pisun}" height="${pisun}" style="background-color:${getRandomHexColor()};" > afssdfsasafa</div>`);
+  }
+}
+
+function destroyBoxes() {
+  box.remove();
+  table.insertAdjacentHTML("afterend", `<div id="boxes"></div>`);
+}
+
+
+createBtn.addEventListener("click", () => {
+  let size = input.value;
+  createBoxes(size);
+  box.innerHTML = array.join("");
+});
+
+ 
+clearBtn.addEventListener("click", () => {
+  destroyBoxes();
+});
